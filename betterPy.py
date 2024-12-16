2b42330a514bd273d6e141f70f724d3869bb05dc
Riley-Code5 <24r.waghorn@thelenham.viat.org.uk>
2024-12-16 12:44:04 +0000

    Add draw class

---
+++ betterPy.py
@@ -0,0 +1,8 @@
+import turtle
+
+class draw:
+  def line(self, outline, pensize, angle, length):
+    turtle.color(outline)
+    turtle.pensize(pensize)
+    turtle.right(angle)
+    turtle.forward(length)
